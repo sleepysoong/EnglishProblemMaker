f1f13063332fe4c2f14fcbd5381ee8f503101904
Seize The Day <42131095+sleepysoong@users.noreply.github.com>
2024-04-30 02:34:04 +0900

    ♻️ 0.0.3 Release : 순서 문제 로직을 전면 개편
    비효율적인 코드를 개선하고 기존에는 A, B, C로 나눈 후 정답에 A --> C --> B와 같이 순서를 단순히 출력해줬다면,
    
    실제 문제처럼 선지까지 랜덤으로 생성하여 오지선다 중 고를 수 있도록 로직을 전면 개편하였다.

--- maker.py
+++ maker.py
@@ -3,9 +3,13 @@ import os
 #import json
 import random
 from datetime import datetime
+import itertools
 
-VERSION = [0, 0, 2]
+
+VERSION = [0, 0, 3]
+
 NUMBER_SYMBOLS = ["⓪", "①", "②", "③", "④", "⑤", "⑥", "⑦", "⑧", "⑨", "⑩", "⑪", "⑫", "⑬", "⑭", "⑮", "⑯", "⑰", "⑱", "⑲", "⑳", "㉑", "㉒", "㉓", "㉔", "㉕", "㉖", "㉗", "㉘", "㉙", "㉚", "㉛", "㉜", "㉝", "㉞", "㉟", "㊱", "㊲", "㊳", "㊴", "㊵", "㊶", "㊷", "㊸", "㊹", "㊺", "㊻", "㊼", "㊽", "㊾", "㊿"]
+ORDER_CANDIDATES = ["A", "B", "C"]
 
 def writeTxt(fileName, givenName, content):
     name = "생성된 문제 파일/" + givenName + " - " + fileName + " @SleepySoong.txt"
@@ -59,40 +63,28 @@ def beVerbVariation(originalContent):
         content = content.replace(" " + verb + " ", " ***" + beVerbList_pastTense[random.randint(0, len(beVerbList_pastTense) - 1)] + "*** ")
     return content
 
-def orderVariation(originalContent):
-    if(originalContent == ""):
-        return "본문을 입력하지 않았습니다"
-    content = originalContent.split(".")
-    if "" in content:
-        content.remove("")
-    if(len(content) < 6):
-        return "7개 이상의 문장으로 구성되어 있는 지문만 순서 문제를 제작할 수 있습니다"
-    firstMassCut = round(len(content) / 3)
-    secondMassCut = firstMassCut * 2
-    firstMassContent = ""
-    for firstIndex in range(0, firstMassCut):
-        firstMassContent = firstMassContent + content[firstIndex] + "."
-    secondMassContent = ""
-    for secondIndex in range(firstMassCut + 1, secondMassCut):
-        secondMassContent = secondMassContent + content[secondIndex] + "."
-    thirdMassContent = ""
-    for thirdIndex in range(secondMassCut + 1, len(content)):
-        thirdMassContent = thirdMassContent + content[thirdIndex] + "."
-    contentList = [firstMassContent, secondMassContent, thirdMassContent]
-    shuffledList = contentList.copy()
-    random.shuffle(shuffledList)
-    firstOrder = 0
-    secondOrder = 0
-    thirdOrder = 0
-    for i in range(len(shuffledList)):
-        if shuffledList[i] == contentList[0]:
-            firstOrder = ['A', 'B', 'C'][i]
-        elif shuffledList[i] == contentList[1]:
-            secondOrder = ['A', 'B', 'C'][i]
-        else:
-            thirdOrder = ['A', 'B', 'C'][i]
-
-    return "[A] " + shuffledList[0] + "\n\n[B] " + shuffledList[1] + "\n\n[C] " + shuffledList[2] + "\n\n\n** 정답: " + firstOrder + " --> " + secondOrder + " --> " + thirdOrder
+def orderVariation(text):
+    def convertNumToStr(option: list) -> list: # 숫자로 구성된 배열 순서 정보가 담긴 리스트를 문자(A, B, C)로 변경해주는 함수
+        return [ORDER_CANDIDATES[option.index(0)], ORDER_CANDIDATES[option.index(1)], ORDER_CANDIDATES[option.index(2)]]
+    def convertListToStr(option: list) -> str: # 선지 정보가 담긴 리스트를 문자 (예시; (A) - (C) - (B))로 변경해주는 함수
+        return f"({option[0]}) - ({option[1]}) - ({option[2]})"
+    sentences = convertText(text)
+    firstSentence = sentences.pop(0) # 첫 번째 문장을 저장해두고 리스트에서 제거 함
+    count = len(sentences) # 문장의 갯수
+    criterion = int(count/3) # 한 덩어리에 몇 문장을 넣어야 할지 정함 (한 덩어리에 criterion 문장)
+    mass = [sentences[0:criterion], sentences[criterion:criterion*2], sentences[criterion*2:]] # criterion을 기준으로 문장을 3 덩어리로 나눠서 저장
+    temp = list(range(3)) # 3 덩어리를 섞기 위한 작업 시작
+    random.shuffle(temp) # 0, 1, 2 -> x, y, z (덩어리 섞기)
+    mass[0], mass[1], mass[2] = mass[temp[0]], mass[temp[1]], mass[temp[2]] # 섞기
+    optionPermutations = list(itertools.permutations(list(range(0, 3)))) # 선지를 만들기 위해 0, 1, 2로 만들 수 있는 모든 조합 찾기
+    optionPermutations.remove(tuple(temp)) # 정답을 제외하고 선지에 넣을 나머지 4개를 찾을 계획
+    random.shuffle(optionPermutations) # 랜덤으로 4개를 찾기 위해 섞음
+    options = [temp] + list(map(list, optionPermutations[0:4])) # 정답 선지 + 랜덤 선지 4개
+    options.sort() # 선지 배열
+    options = list(map(convertNumToStr, map(list, options))) # 숫자를 영어로 변환
+    correctOrder = convertNumToStr(temp) # 정답 선지
+    print(options)
+    return "주어진 글 다음에 이어질 글의 순서로 가장 적절한 것을 고르시오.\n\n[ " + str(firstSentence) + " ]\n\n\n(A) " + ". ".join(mass[0]) + "\n\n(B) " + ". ".join(mass[1]) + "\n\n(C) " + ". ".join(mass[2]) + "\n\n\n" + NUMBER_SYMBOLS[1] + convertListToStr(options[0]) + "\n" + NUMBER_SYMBOLS[2] + convertListToStr(options[1]) + "\n" + NUMBER_SYMBOLS[3] + convertListToStr(options[2]) + "\n" + NUMBER_SYMBOLS[4] + convertListToStr(options[3]) + "\n" + NUMBER_SYMBOLS[5] + convertListToStr(options[4]) + "\n\n\n** 정답: " + NUMBER_SYMBOLS[options.index(correctOrder) + 1]
 
 def blankVariation(originalContent):
     if(originalContent == ""):
